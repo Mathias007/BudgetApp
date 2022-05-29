3ef79ebb6c8dfc4387cbd4a1fd6c6c5fb6e1c33c
Mathias007 <mathias.najlepszy@gmail.com>
2022-05-29 21:43:54 +0200

    Start building transactions system using Program Main function and generating first TransactionsList.json as example.

--- BudgetApp/Program.cs
+++ BudgetApp/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace BudgetApp
 {
     class Program
     {
-        static void Main(string[] args)
+        public static async Task Main()
         {
-            Console.WriteLine("Hello World!");
+            Budget budget = new();
+
+            budget.CreateBudgetDB();
+
+            Transaction firstTransaction = new(0,
+                 new Category("income", "transaction"),
+                 2137.5,
+                 "Testowa transakcja",
+                 new User("Jan", "Kowalski", true),
+                 DateTime.Parse("2019-08-01"));
+
+            await firstTransaction.AddTransactionToDB(firstTransaction);
+
+            // await budget.ShowTransactionList();
+
         }
     }
 }
