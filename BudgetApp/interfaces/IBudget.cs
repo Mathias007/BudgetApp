394017195567d792ea4701e4a028387323a7591a
Mathias007 <mathias.najlepszy@gmail.com>
2022-05-29 21:38:04 +0200

    Prepare interfaces for Budget and BudgetService classes.

---
+++ BudgetApp/interfaces/IBudget.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetApp
+{
+    interface IBudget
+    {
+        string SelectedMonth { get; set; } 
+        double Balance { get; set; }
+        double[] BudgetStructure { get; set; }
+
+    }
+}
---
+++ BudgetApp/interfaces/IBudgetService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetApp
+{
+    interface IBudgetService
+    {
+        string GetDatabasePath();
+        void CreateBudgetDB();
+        Task ShowTransactionList();
+        double CalculateBalanceValue();
+        double CalculateBudgetStructure();
+    }
+}
