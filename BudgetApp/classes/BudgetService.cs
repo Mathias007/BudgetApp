e2469c140fd536f25a0f1abdb511a7976483a9a2
Mathias007 <mathias.najlepszy@gmail.com>
2022-05-29 21:39:29 +0200

    Create BudgetService abstract class implementing IBudgetService interface. Fill some primary methods.

---
+++ BudgetApp/classes/BudgetService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace BudgetApp
+{
+    abstract class BudgetService : IBudgetService
+    {
+        public string GetDatabasePath()
+        {
+            string fileName = "TransactionsList.json";
+            return Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, $"..\\..\\..\\{fileName}"));
+        }
+        public void CreateBudgetDB()
+        {
+            using FileStream createStream = File.Create(GetDatabasePath());
+        }
+        public async Task ShowTransactionList() 
+        {
+            using FileStream openStream = File.OpenRead(GetDatabasePath());
+            Transaction[]? transactionsList =
+                await JsonSerializer.DeserializeAsync<Transaction[]>(openStream);
+
+            //Console.WriteLine($"ID: {transaction?.TransactionID}");
+            //Console.WriteLine($"Category: {transaction?.TransactionCategory}");
+            //Console.WriteLine($"Date: {transaction?.TransactionDate}");               
+        }
+        public double CalculateBalanceValue() => throw new NotImplementedException();
+        public double CalculateBudgetStructure() => throw new NotImplementedException();
+    }
+}
