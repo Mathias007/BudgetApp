07c13ca4ac54a55e9cc90962c7bd63b6cfc97a5d
Mathias007 <mathias.najlepszy@gmail.com>
2022-06-04 22:15:57 +0200

    Build IMenu interface.

---
+++ BudgetApp/interfaces/IMenu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetApp
+{
+    interface IMenu
+    {
+        bool IsProgramOpen { get; set; }
+        Dictionary<string,string> ProgramOptions { get; }
+        void ShowUsersList(Dictionary<int, User> usersList);
+        void AddTransactions(Dictionary<int, Transaction> transactionsList, Dictionary<int, Category> categoriesList, User user);
+        void EditTransactions(Dictionary<int, Transaction> transactionsList, Dictionary<int, Category> categoriesList, User user);
+        void ShowTransactions(Dictionary<int, Transaction> transactionsList, Dictionary<int, Category> categoriesList, User user);
+        void HandleMenu(Dictionary<int, User> usersList, Dictionary<int, Transaction> transactionsList, Dictionary<int, Category> categoriesList, User user);
+        void ManageProgramWorking();
+    }
+}
