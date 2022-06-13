b06b94e2f6214bf58c35b98409124a1b96e3cc6a
Mathias007 <mathias.najlepszy@gmail.com>
2022-06-13 17:56:42 +0200

    Move categories handling methods to Category.cs and users to User.cs, make some corrects.

--- BudgetApp/classes/User.cs
+++ BudgetApp/classes/User.cs
@@ -29,5 +29,83 @@ namespace BudgetApp
             _isActive = isActive;
             _isAdmin = isAdmin;
         }
+
+        public static void PrintUsers(bool onlyActive, Dictionary<int, User> usersList)
+        {
+            foreach (KeyValuePair<int, User> record in usersList)
+            {
+                if (!onlyActive || record.Value.IsActive)
+                {
+                    Console.WriteLine(
+                        $" + [{record.Key}]: " +
+                        $"{record.Value.UserFirstName} {record.Value.UserLastName} " +
+                        $"{(record.Value.UserIsActive ? "AKTYWNY" : "NIEAKTYWNY")} " +
+                        $"{(record.Value.UserIsAdmin ? "ADMINISTRATOR" : "USER")} ");
+                }
+            }
+        }
+
+        public static void ManageUsers(Dictionary<int, User> usersList)
+        {
+            Console.Clear();
+            Console.WriteLine("Lista wszystkich domowników:");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine(" + [0]: dodaj nowego domownika");
+            PrintUsers(false, usersList);
+            Console.WriteLine(" \n -> Wybierz 0, aby dodać nowego domownika. \n -> Jeżeli chcesz zmodyfikować dane istniejącego domownika, wypisz jego numer ID. \n -> Aby wrócić do głównego menu, naciśnij ENTER, pozostawiając pole puste.");
+            int consoleID = GetConsoleInput<User>.GetUserInputID(usersList, false);
+            if (consoleID == -1)
+                Console.WriteLine(" + [0]: dodaj nowego domownika");
+            foreach (KeyValuePair<int, User> record in usersList)
+            {
+                Console.WriteLine(
+                    $" + [{record.Key}]: " +
+                    $"{record.Value.UserFirstName} {record.Value.UserLastName} " +
+                    $"{(record.Value.UserIsActive ? "AKTYWNY" : "NIEAKTYWNY")} " +
+                    $"{(record.Value.UserIsAdmin ? "ADMINISTRATOR" : "USER")} ");
+            }
+
+            Console.WriteLine(" \n -> Wybierz 0, aby dodać nowego domownika. \n -> Jeżeli chcesz zmodyfikować dane istniejącego domownika, wypisz jego numer ID. \n -> Aby wrócić do głównego menu, naciśnij ENTER, pozostawiając pole puste.");
+            string userInput = Console.ReadLine();
+
+            if (String.IsNullOrWhiteSpace(userInput))
+
+            {
+                Console.Clear();
+                return;
+            }
+            if (consoleID == 0)
+            {
+                int newUserID = usersList.Count == 0 ? 1 : usersList.Keys.Max() + 1;
+                Console.WriteLine("Imię: ");
+                string firstName = Console.ReadLine();
+                Console.WriteLine("Nazwisko: ");
+                string lastName = Console.ReadLine();
+                User addingUser = new(newUserID, firstName, lastName);
+                usersList.Add(addingUser.UserID, addingUser);
+                return;
+            }
+            Console.Clear();
+
+            Console.WriteLine($"Wpisz nowe imię, zostaw puste żeby pominiąć({usersList[consoleID].UserFirstName}): ");
+            string newFirstName = Console.ReadLine();
+            usersList[consoleID].UserFirstName = String.IsNullOrWhiteSpace(newFirstName) ? usersList[consoleID].UserFirstName : newFirstName;
+            Console.Clear();
+            Console.WriteLine($"Wpisz nowe nazwisko, zostaw puste żeby pominiąć({usersList[consoleID].UserLastName}): ");
+            string newLastName = Console.ReadLine();
+            usersList[consoleID].UserLastName = String.IsNullOrWhiteSpace(newLastName) ? usersList[consoleID].UserLastName : newLastName;
+            Console.Clear();
+            Console.WriteLine($"Domownik jest aktywny({usersList[consoleID].UserIsActive})? (t/n), zostaw puste żeby nie zmieniać");
+            string newActiveStatus = Console.ReadLine().ToUpper();
+
+            if (newActiveStatus.Equals("T"))
+                usersList[consoleID].UserIsActive = true;
+            else if (newActiveStatus.Equals("N"))
+                usersList[consoleID].UserIsActive = false;
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("\n");
+        }
     }
 }
