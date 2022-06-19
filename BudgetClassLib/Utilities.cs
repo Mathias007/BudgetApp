8e6d2da9c93494caa203d4d7e40e12bca7e8ff1b
Mathias007 <mathias.najlepszy@gmail.com>
2022-06-19 14:50:59 +0200

    Create BudgetClassLib with primary utils.

---
+++ BudgetClassLib/Utilities.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace UtilityLibraries
+{
+    public class UtilitiesLibrary
+    {
+        public static string GetDatabasePath(string fileName)
+        {
+            return Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, $"..\\..\\..\\{fileName}"));
+        }
+
+        public static int RandomizeNumber(int min, int max) => new Random().Next(min, max);
+    }
+}
